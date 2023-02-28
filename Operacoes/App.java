947df5a55e12d2a094b2bc51f3a5b561071d388c
Diogotb <digogo@gmail.com>
2023-02-28 17:19:06 -0300

    28/02/23

---
+++ Operacoes/App.java
@@ -0,0 +1,10 @@
+package Operacoes;
+
+public class App {
+    public static void main(String[] args) {
+        Operadores objeto1 = new Operadores();
+        objeto1.aritmetica();
+        Operadores objeto2 = new Operadores();
+        objeto2.atribuicao();
+    }
+}
