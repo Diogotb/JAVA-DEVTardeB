21220792c16f79912e84022e76ccac9250281261
Diogotb <digogo@gmail.com>
2023-08-03 17:33:19 -0300

    03/08

---
+++ Exception/ExemploThrow.java
@@ -0,0 +1,33 @@
+package Exception;
+
+import javax.swing.JOptionPane;
+
+/**
+ * ExemploThrow
+ */
+public class ExemploThrow {
+
+    public static void main(String[] args) {
+        //
+        try {
+            JOptionPane.showMessageDialog(null, "Welcome to SENAI");
+            String login = JOptionPane.showInputDialog("Informe seu Login");
+            String dataNascimento = JOptionPane.showInputDialog("Informe sua Data de Nascimento [dd/mm/aa]");
+            dataNascimento=dataNascimento.replace("/", "");
+            System.out.println(dataNascimento);
+            String senha = JOptionPane.showInputDialog("Informe sua Senha de 6 Digitos");
+            
+            if (senha.length() != 6) {
+                throw new Exception("Senha deve ter 6 Digitos");
+            }else if(senha.equals(login)){
+                throw new Exception("Senha igual Login");
+            }else if(senha.equals(dataNascimento)){
+                throw new Exception("Senha igual Data de Nascimento");
+            }
+            
+        } catch (Exception e) {
+            // TODO: handle exception
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
+}
--- Exception/ExemploTryCatch.java
+++ Exception/ExemploTryCatch.java
@@ -6,7 +6,7 @@ public class ExemploTryCatch {
     public static void main(String[] args) {
         // tratando as exceptions
         boolean calcular = true;
-        int n1=0, n2=0;
+        int n1 = 0, n2 = 0;
         while (calcular) {
             try {
                 // recebendo os dados
@@ -18,7 +18,6 @@ public class ExemploTryCatch {
                     } catch (Exception e) {
                         JOptionPane.showMessageDialog(null, "Erro - valor digitado n~ é inteiro");
                     }
-
                 }
                 boolean n2ok = true;
                 while (n2ok) {
@@ -30,11 +29,31 @@ public class ExemploTryCatch {
                     }
                 }
                 // mostrando o resultado
-                JOptionPane.showMessageDialog(null, "O Resultado é "
-                        + (n1 + n2));
-                calcular = false;
-            } catch (Exception erro1) {
-                JOptionPane.showMessageDialog(null, "Erro - valor digitadno não é inteiro6");
+                // escolher o tipo de conta
+                String operacao[] = { "Adição", "Subtração", "Multiplicação", "Divisão", "Sair" };
+                int acao = JOptionPane.showOptionDialog(null, "Escolha a Operação", "Calculadora",
+                        JOptionPane.DEFAULT_OPTION, JOptionPane.CANCEL_OPTION, null, operacao, operacao[0]);
+                if (acao == 0) {
+                    JOptionPane.showMessageDialog(null, "O Resultado é "
+                            + (n1 + n2));
+                } else if (acao == 1) {
+                    JOptionPane.showMessageDialog(null, "O Resultado é "
+                            + (n1 - n2));
+                } else if (acao == 2) {
+                    JOptionPane.showMessageDialog(null, "O Resultado é "
+                            + (n1 * n2));
+                } else if (acao == 3) {
+                    JOptionPane.showMessageDialog(null, "O Resultado é "
+                            + (n1 / n2));
+                } else {
+                    calcular = false;
+                }
+            } catch (NumberFormatException erro1) {
+                JOptionPane.showMessageDialog(null, "Erro - valor digitadno não é inteiro");
+            } catch (ArithmeticException erro2) {
+                JOptionPane.showMessageDialog(null, "Erro - não dividirás por Zero");
+            } catch (Exception erro3) {
+                JOptionPane.showMessageDialog(null, "Erro - ");
             }
         }
     }
