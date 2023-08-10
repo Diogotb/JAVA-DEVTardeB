37622794c6c033cbdbcaa2e7059cd1325b2dcba1
Diogotb <digogo@gmail.com>
2023-08-10 18:53:41 -0300

    10/08

---
+++ AWTPrimeiroFrame/src/ExemploJFrame.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class ExemploJFrame extends JFrame{
+    public ExemploJFrame() {
+        super();
+        this.setSize(100, 300);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+
+    }
+}
