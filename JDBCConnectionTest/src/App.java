ce420b5af9b01cbdd9f31b96ccc0ba2d5ad8ee62
Diogotb <digogo@gmail.com>
2023-10-31 18:01:43 -0300

    31/10

---
+++ JDBCConnectionTest/src/App.java
@@ -0,0 +1,10 @@
+
+
+import Connection.ConnectionDAO;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        new ConnectionDAO().criaTabela();
+        new ConnectionDAO().inserir("Diogo","diogo@gmail.com");
+    }
+}
---
+++ JDBCConnectionTest/src/Connection/ConnectionFactory.java
@@ -0,0 +1,54 @@
+package Connection;
+
+import java.beans.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    // atributos
+    private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USUARIO = "postgres";
+    private static final String SENHA = "postgres";
+
+    // métodos
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USUARIO, SENHA);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao obter conexão com o banco de dados.", e);
+        }
+    }
+
+    public static void closeConnection(Connection connection) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+    public static void closeConnection(PreparedStatement stmt) {
+        try {
+            if (stmt != null && stmt != null) {
+                stmt.close();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+     public static void closeConnection(Connection connection, PreparedStatement stmt) {
+        try {
+            if (connection != null && stmt != null) {
+                stmt.close();
+                connection.close();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+
+}
