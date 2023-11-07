2861c72cecaeae8a6ac439dcaf2d2f7c2ea22f14
Diogotb <digogo@gmail.com>
2023-11-07 17:50:24 -0300

    07/11

--- JDBCConnectionTest/src/Connection/ConnectionDAO.java
+++ JDBCConnectionTest/src/Connection/ConnectionDAO.java
@@ -5,32 +5,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ConnectionDAO {
-    // atributo
+    // atributo para manter a conexão com o banco de dados
     private Connection connection;
 
-    // construtor
+    // Construtor da classe
     public ConnectionDAO() {
-        this.connection = ConnectionFactory.getConnection();
+        this.connection = ConnectionFactory.getConnection(); // Inicializa a conexão no construtor
     }
 
-    // métodos
+    // Método para criar a tabela no banco de dados
     public void criaTabela() {
-
-        String sql = "CREATE TABLE IF NOT EXISTS MINHA_TABELA (ID SERIAL PRIMARY KEY,NOME VARCHAR(255),EMAIL VARCHAR(255))";
+        // Define a instrução SQL para criar a tabela MINHA_TABELA se não existir
+        String sql = "CREATE TABLE IF NOT EXISTS MINHA_TABELA (ID SERIAL PRIMARY KEY,NOME VARCHAR(255),EMAIL VARCHAR(255)";
         try (Statement stmt = this.connection.createStatement()) {
-            stmt.execute(sql);
+            stmt.execute(sql); // Executa a instrução SQL
             System.out.println("Tabela criada com sucesso.");
         } catch (SQLException e) {
+            // Captura exceções relacionadas a erros no banco de dados
             throw new RuntimeException("Erro ao criar a tabela: " + e.getMessage(), e);
         } finally {
-            ConnectionFactory.closeConnection(this.connection);
+            ConnectionFactory.closeConnection(this.connection); // Fecha a conexão
         }
     }
 
+    // Método para apagar a tabela do banco de dados
     public void apagarTabela() {
+        // Define a instrução SQL para apagar a tabela Minha_Tabela
         String sql = "DROP TABLE Minha_Tabela";
         try (Statement stmt = connection.createStatement()) {
-            stmt.executeUpdate(sql);
+            stmt.executeUpdate(sql); // Executa a instrução SQL
             System.out.println("Tabela apagada com sucesso.");
         } catch (Exception e) {
             throw new RuntimeException("Erro ao apagar tabela.", e);
@@ -39,26 +42,15 @@ public class ConnectionDAO {
         }
     }
 
+    // Método para inserir dados na tabela com ID especificado
     public void inserir(int id, String nome, String email) {
-        String sql = "INSERT INTO MINHA_TABELA (ID,NOME, EMAIL) VALUES (?, ?, ?)";
+        // Define a instrução SQL parametrizada para inserir dados com ID
+        String sql = "INSERT INTO MINHA_TABELA (ID, NOME, EMAIL) VALUES (?, ?, ?)";
         try (PreparedStatement stmt = connection.prepareStatement(sql)) {
             stmt.setInt(1, id);
             stmt.setString(2, nome);
             stmt.setString(3, email);
-            stmt.executeUpdate();
-            System.out.println("Dados inseridos com sucesso");
-        } catch (SQLException e) {
-            throw new RuntimeException("Erro ao inserir dados no banco de dados.", e);
-        } finally {
-            ConnectionFactory.closeConnection(this.connection);
-        }
-    }
-    public void inserir(String nome, String email) {
-        String sql = "INSERT INTO MINHA_TABELA (NOME, EMAIL) VALUES (?, ?)";
-        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
-            stmt.setString(1, nome);
-            stmt.setString(2, email);
-            stmt.executeUpdate();
+            stmt.executeUpdate(); // Executa a instrução SQL
             System.out.println("Dados inseridos com sucesso");
         } catch (SQLException e) {
             throw new RuntimeException("Erro ao inserir dados no banco de dados.", e);
@@ -67,47 +59,14 @@ public class ConnectionDAO {
         }
     }
 
-    public void buscarPorId(int id) {
-        String sql = "SELECT * FROM MINHA_TABELA WHERE ID = ?";
+    // Método para inserir dados na tabela sem especificar um ID
+    public void inserir(String nome, String email) {
+        // Define a instrução SQL parametrizada para inserir dados sem ID
+        String sql = "INSERT INTO MINHA_TABELA (NOME, EMAIL) VALUES (?, ?)";
         try (PreparedStatement stmt = connection.prepareStatement(sql)) {
-            stmt.setInt(1, id);
-            ResultSet resultSet = stmt.executeQuery();
-            while (resultSet.next()) {
-                int idBuscado = resultSet.getInt("ID");
-                String nomeBuscado = resultSet.getString("NOME");
-                String emailBuscado = resultSet.getString("EMAIL");
-                System.out.println(
-                        "o Resultado da busca é id " + idBuscado + " nome " + nomeBuscado + " email " + emailBuscado);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException("Erro ao buscar dados no banco de dados.", e);
-        } finally {
-            ConnectionFactory.closeConnection(this.connection);
-        }
-    }
-    // Apagar um dado da Tabela
-    public void apagarID(int id){
-        String sql = "DELETE FROM Minha_Tabela WHERE ID = ?";
-        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
-            stmt.setInt(1, id);
-            stmt.executeUpdate();
-            System.out.println("Dado apagado com sucesso");
-            
-        } catch (SQLException e) {
-            throw new RuntimeException("Erro ao buscar dados no banco de dados.", e);
-        } finally {
-            ConnectionFactory.closeConnection(this.connection);
-        }
-    }
-    //Atualizar pelo ID
-    public void atualizarID(int id, String novoNome, String novoEmail){
-        String sql = "UPDATE MINHA_TABELA SET nome = ? , email = ? WHERE ID = ?";
-        //UPDATE nomeDaTabela SET (Dados que serão alterados) WHERE (índice da busca)
-        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
-            stmt.setString(1, novoNome);
-            stmt.setString(2, novoEmail);
-            stmt.setInt(3, id);
-            stmt.executeUpdate();
+            stmt.setString(1, nome);
+            stmt.setString(2, email);
+            stmt.executeUpdate(); // Executa a instrução SQL
             System.out.println("Dados inseridos com sucesso");
         } catch (SQLException e) {
             throw new RuntimeException("Erro ao inserir dados no banco de dados.", e);
@@ -115,17 +74,71 @@ public class ConnectionDAO {
             ConnectionFactory.closeConnection(this.connection);
         }
     }
-    //Listar todos os valores cadastrados
-    public void listarTodos() { // Colocar na tabela
+
+    // Método para buscar dados na tabela com base em um ID específico
+    public void buscarPorId(int id) {
+        // Define a instrução SQL parametrizada para buscar dados por ID
+        String sql = "SELECT * FROM MINHA_TABELA WHERE ID = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, id);
+            ResultSet resultSet = stmt.executeQuery(); // Executa a consulta e obtém resultados
+            while (resultSet.next()) {
+                int idBuscado = resultSet.getInt("ID");
+                String nomeBuscado = resultSet.getString("NOME");
+                String emailBuscado = resultSet.getString("EMAIL");
+                System.out.println(
+                        "O resultado da busca é id " + idBuscado + " nome " + nomeBuscado + " email " + emailBuscado);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao buscar dados no banco de dados.", e);
+        } finally {
+            ConnectionFactory.closeConnection(this.connection);
+        }
+    }
+
+    // Método para apagar um dado da tabela com base em um ID específico
+    public void apagarID(int id) {
+        // Define a instrução SQL parametrizada para apagar dados por ID
+        String sql = "DELETE FROM Minha_Tabela WHERE ID = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setInt(1, id);
+            stmt.executeUpdate(); // Executa a instrução SQL
+            System.out.println("Dado apagado com sucesso");
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao apagar dados no banco de dados.", e);
+        } finally {
+            ConnectionFactory.closeConnection(this.connection);
+        }
+    }
+
+    // Método para atualizar dados na tabela com base em um ID específico
+    public void atualizarID(int id, String novoNome, String novoEmail) {
+        // Define a instrução SQL parametrizada para atualizar dados por ID
+        String sql = "UPDATE MINHA_TABELA SET nome = ?, email = ? WHERE ID = ?";
+        try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+            stmt.setString(1, novoNome);
+            stmt.setString(2, novoEmail);
+            stmt.setInt(3, id);
+            stmt.executeUpdate(); // Executa a instrução SQL
+            System.out.println("Dados atualizados com sucesso");
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao atualizar dados no banco de dados.", e);
+        } finally {
+            ConnectionFactory.closeConnection(this.connection);
+        }
+    }
+
+    // Método para listar todos os valores cadastrados na tabela
+    public void listarTodos() {
         ResultSet rs = null;
+        // Define a instrução SQL para selecionar todos os registros da tabela
         String sql = "SELECT * FROM minha_tabela";
         try (PreparedStatement stmt = connection.prepareStatement(sql)) {
-            rs = stmt.executeQuery();
+            rs = stmt.executeQuery(); // Executa a consulta e obtém resultados
             while (rs.next()) {
-                
-                System.out.println("id : "+rs.getInt("id")+
-                " nome: "+rs.getString("nome")+
-                " email: "+rs.getString("email")); 
+                System.out.println("id : " + rs.getInt("id") +
+                        " nome: " + rs.getString("nome") +
+                        " email: " + rs.getString("email"));
             }
         } catch (SQLException ex) {
             System.out.println(ex);
